b64054614e3b7a8052bc03ed3e805cb2c45ba283
송규진 <songu9610@gmail.com>
2023-08-16 08:59:05 +0900

    fix. 오류나는 레이아웃 수정. 회원가입 로직 수정 feat. 다이얼로그 구현 및 코드 객체화

--- app/src/main/java/com/example/nbcamp_15ya_sns/SiginUpActivity.kt
+++ app/src/main/java/com/example/nbcamp_15ya_sns/SiginUpActivity.kt
@@ -1,13 +1,14 @@
 package com.example.nbcamp_15ya_sns
 
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.UserManager
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 
@@ -20,6 +21,7 @@ class SiginUpActivity : AppCompatActivity() {
     private val stateM: EditText by lazy { findViewById(R.id.profile_ed) }
 
     private var nameCheck: Boolean = false
+    private var mbtiCheck: Boolean = false
 
     //지연초기화
     // 이 변수가 사용되는 시점에서 초기화가 된다.
@@ -27,22 +29,27 @@ class SiginUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sigin_up_activity)
 
+        mbtiWatcher(mbti)
+
 
     }
 
-    fun doubleCheck(view: View) {
-        if (userManager.userList.find { it.id == id.text.toString() } != null) {
+    fun doubleCheckOnClick(view: View) {
 
-            Toast.makeText(getApplicationContext(), "중복된 아이디 입니다!!", Toast.LENGTH_SHORT).show()
+        val inputid = id.text.toString()
+        if (userManager.userList.find { it.id == inputid } != null
+            || inputid.isEmpty()) {
+
+           toast("불가능한 아이디입니다.")
             nameCheck = false
 
-        } else {
-            Toast.makeText(getApplicationContext(), "사용가능한 아이디 입니다!!", Toast.LENGTH_SHORT).show()
+        } else{
+            toast("사용 가능한 아이디입니다.")
             nameCheck = true
         }
     }
 
-    fun signupButton(view: View) {
+    fun signupOnClick(view: View) {
 
         val inputId = id.text.toString()
         val inputpsw = psw.text.toString()
@@ -51,12 +58,10 @@ class SiginUpActivity : AppCompatActivity() {
         val inputstateM = stateM.text.toString()
 
         when {
-            nameCheck == false ->
-                Toast.makeText(getApplicationContext(), "아이디 중복 체크를 해주세요!!", Toast.LENGTH_SHORT)
-                    .show()
+            nameCheck == false -> toast("아이디 중복 체크를 해주세요!!")
 
             checkEmpty(inputpsw) || checkEmpty(inputname) || checkEmpty(inputmbti)
-            -> checkMessage(psw = inputpsw, name = inputname, mbti = inputmbti)
+            -> checkMessage(inputPsw = inputpsw, inputName = inputname, inputMBTI = inputmbti)
 
             else -> {
                 userManager.userList.add(
@@ -75,26 +80,29 @@ class SiginUpActivity : AppCompatActivity() {
 
     }
 
-    fun finish(view: View) {
+    fun finishOnClick(view: View) {
         finish()
     }
 
-    fun checkMessage(psw: String, name: String, mbti: String) {
+
+
+    fun checkMessage(inputPsw: String, inputName: String, inputMBTI: String) {
 
         when {
-            checkEmpty(psw) -> toast("비밀번호를 확인해주세요")
 
-            checkEmpty(name) -> toast("이름을 입력해주세요")
+            checkEmpty(inputPsw) && checkEmpty(inputName) && checkEmpty(inputMBTI) -> toast("소개글을 제외한 공백을 확인해주세요")
 
-            checkEmpty(mbti) -> toast("mbti를 입력해주세요")
+            checkEmpty(inputPsw) && checkEmpty(inputName) -> toast("비밉번호와 이름을 입력해주세요")
 
-            checkEmpty(psw) && checkEmpty(name) -> toast("비밉번호와 이름을 입력해주세요")
+            checkEmpty(inputPsw) && checkEmpty(inputMBTI) -> toast("비밉번호와 mbti를 입력해주세요")
 
-            checkEmpty(psw) && checkEmpty(mbti) -> toast("비밉번호와 mbti를 입력해주세요")
+            checkEmpty(inputMBTI) && checkEmpty(inputName) -> toast("mbti와 이름을 입력해주세요")
 
-            checkEmpty(mbti) && checkEmpty(name) -> toast("mbti와 이름을 입력해주세요")
+            checkEmpty(inputPsw) -> toast("비밀번호를 확인해주세요")
 
-            checkEmpty(psw) && checkEmpty(name) && checkEmpty(mbti) -> toast("공백을 확인해주세요 입력해주세요")
+            checkEmpty(inputName) -> showDialog("이름",name)
+
+            checkEmpty(inputMBTI) -> showMBTIDialog("MBTI")
 
         }
 
@@ -109,7 +117,7 @@ class SiginUpActivity : AppCompatActivity() {
     }
 
     fun isMBTI(mbti: String): Boolean {
-        return mbti.matches(Regex("[EI][FT][JP]]"))
+        return mbti.matches(Regex("[EI][SN][TF][JP]"))
     }
 
     fun mbtiWatcher(view: View) {
@@ -123,8 +131,74 @@ class SiginUpActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(s: Editable?) {
+                val text:TextView = findViewById(R.id.mbti_check_text)
+                if(!isMBTI(s.toString())){
+                    text.text = "잘못된 MBTI입니다."
+                    mbtiCheck = false
+                }else{
+                    text.text = "올바른 MBTI입니다."
+                    mbtiCheck = true
+                }
 
             }
         })
     }
+
+    fun showMBTIDialog(title: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val dialogView = inflater.inflate(R.layout.custom_mbti_dialog, null)
+        val input: EditText = dialogView.findViewById(R.id.custom_edit_text)
+
+        builder.setTitle(title)
+        builder.setView(dialogView)
+
+        builder.setPositiveButton("확인") { dialog, _ ->
+            val inputText = input.text.toString()
+
+            if (isMBTI(inputText)) mbti.setText(inputText)
+            else toast("올바른 mbti를 입력해주세요")
+
+            dialog.dismiss()
+        }
+
+        builder.setNegativeButton("취소") { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    fun showDialog(title: String,id:EditText) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val dialogView = inflater.inflate(R.layout.custom_text_dialog, null)
+        val input: EditText = dialogView.findViewById(R.id.costom_text_edit)
+
+        builder.setTitle(title)
+        builder.setView(dialogView)
+
+        builder.setPositiveButton("확인") { dialog, _ ->
+            val inputText = input.text.trim().toString()
+
+            if (inputText.isNotEmpty()) {
+                id.setText(inputText)
+                dialog.dismiss()
+            }else{
+                toast("${title}을 입력해주세요")
+            }
+
+        }
+
+        builder.setNegativeButton("취소") { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+
+
 }
