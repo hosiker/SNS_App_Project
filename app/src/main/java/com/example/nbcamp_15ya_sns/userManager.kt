4271c8d2514090b0fc37a7e7d3cfaaa31d5090f7
choco5732 <choco5732@gmail.com>
2023-08-16 09:24:24 +0900

    success!

--- app/src/main/java/com/example/nbcamp_15ya_sns/userManager.kt
+++ app/src/main/java/com/example/nbcamp_15ya_sns/userManager.kt
@@ -2,7 +2,7 @@ package com.example.nbcamp_15ya_sns
 
 object userManager {
 
-    var userList = mutableListOf<User>()
+    var userList  = mutableListOf<User>()
 
     init {
         userList.add(User("test","test","test","test","test"))
