5d24e2c402c53a4c90db57dabdaae34748d7d196
송규진 <songu9610@gmail.com>
2023-08-17 20:06:19 +0900

    [FEAT] : ⚙️  마이페이지 완 및 오브젝트생성

--- app/src/main/java/com/example/nbcamp_15ya_sns/MyPageActivity.kt
+++ app/src/main/java/com/example/nbcamp_15ya_sns/MyPageActivity.kt
@@ -2,6 +2,9 @@ package com.example.nbcamp_15ya_sns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import de.hdodenhof.circleimageview.CircleImageView
 
 
 class MyPageActivity : AppCompatActivity() {
@@ -10,9 +13,24 @@ class MyPageActivity : AppCompatActivity() {
         setContentView(R.layout.my_page_activity)
 
         val userME = intent.getSerializableExtra("user") as User
-        val myIMG = intent.getSerializableExtra("img")
+        val getImg = intent.getIntExtra("img", 0)
+
+        val nameText = findViewById<TextView>(R.id.my_name)
+        val id = findViewById<TextView>(R.id.my_id)
+        val stateM = findViewById<TextView>(R.id.my_stateM)
+        val mbtiText = findViewById<TextView>(R.id.my_mbti)
+        val faceImg = findViewById<CircleImageView>(R.id.my_photo)
+        val twitImg = findViewById<CircleImageView>(R.id.my_twitPhoto_1)
 
 
+        nameText.text = "${userME.name}"
+        id.text = "$@Id ${userME.id}"
+        stateM.text = "${userME.stateM}"
+        mbtiText.text = "MBTI는 ${userME.mbti}입니다!!"
+
+
+        faceImg.setImageResource(getImg)
+        twitImg.setImageResource(getImg)
 
     }
 }
