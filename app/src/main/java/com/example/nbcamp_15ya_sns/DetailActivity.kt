9dcb904d258b89c7125393aab97de2526ee5254a
이호식 <leehosick462589@gmail.com>
2023-08-17 22:49:18 +0900

    [FEAT] : ⚙️ DetailPage 기능 최종구현

--- app/src/main/java/com/example/nbcamp_15ya_sns/DetailActivity.kt
+++ app/src/main/java/com/example/nbcamp_15ya_sns/DetailActivity.kt
@@ -1,11 +1,13 @@
 package com.example.nbcamp_15ya_sns
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
 import android.text.TextUtils
 import android.text.method.LinkMovementMethod
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
@@ -16,11 +18,13 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.detail_activity)
 
         val linkTextView = findViewById<TextView>(R.id.de_bloglink_text)
-        linkTextView.text = Html.fromHtml("<a href=\"https://velog.io/@ghj6068\">Visit our website</a>")
+        linkTextView.text =
+            Html.fromHtml("<a href=\"https://velog.io/@ghj6068\">Visit our website</a>")
         linkTextView.movementMethod = LinkMovementMethod.getInstance()
         // 벨로그 주소 링크 연결
         val linkTextView2 = findViewById<TextView>(R.id.de_githublink_text)
-        linkTextView2.text = Html.fromHtml("<a href=\"https://github.com/hosiker\">Visit our website</a>")
+        linkTextView2.text =
+            Html.fromHtml("<a href=\"https://github.com/hosiker\">Visit our website</a>")
         linkTextView2.movementMethod = LinkMovementMethod.getInstance()
         // 깃허부 주소 링크 연결
 
@@ -31,7 +35,7 @@ class DetailActivity : AppCompatActivity() {
 
         toggleButton.setOnClickListener {
             if (isExpanded) {
-                contentTextView.maxLines = 3
+                contentTextView.maxLines = 1
                 contentTextView.ellipsize = TextUtils.TruncateAt.END
                 toggleButton.text = "더보기"
             } else {
@@ -42,8 +46,37 @@ class DetailActivity : AppCompatActivity() {
             isExpanded = !isExpanded
         }
 
+        val friend by lazy { intent.getSerializableExtra("friend") as Friend } // friend 라는 객체를 가져와서 friend 변수에에 Friend 형식으로 배치
+
+        val De_name = findViewById<TextView>(R.id.de_name)
+        val De_mbti = findViewById<TextView>(R.id.de_mbti)
+        val De_stateM = findViewById<TextView>(R.id.de_stateM)
+        val De_giturl = findViewById<TextView>(R.id.de_githublink_text)
+        val De_vlourl = findViewById<TextView>(R.id.de_bloglink_text)
+
+        De_name.text = ("${friend.name}")
+        De_mbti.text = ("MBTI는 ${friend.mbti}입니다!!")
+        De_stateM.text = ("${friend.stateM}")
+        De_giturl.text = ("${friend.gitURL}")
+        De_vlourl.text = ("${friend.velURL}")
+        //data 받아와서 설정하기
+
+
     }
 
+    fun moveUserList(v: View) {
+        val moveintent = Intent(this, UserListActivity::class.java)
+        startActivity(moveintent)
+    }
 
+    fun moveHome(v: View) {
+        val moveintent2 = Intent(this, MainActivity::class.java)
+        startActivity(moveintent2)
+    }
+
+    fun moveMypage(v: View) {
+        val moveintent3 = Intent(this, MyPageActivity::class.java)
+        startActivity(moveintent3)
+    }
 
 }
