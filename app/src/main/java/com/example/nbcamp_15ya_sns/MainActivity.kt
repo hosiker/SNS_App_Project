acca478198c75128e118a166efd09da6e297548c
PMETNT <temp19@naver.com>
2023-08-17 09:48:05 +0900

    feat. 마이페이지 레이아웃

--- app/src/main/java/com/example/nbcamp_15ya_sns/MainActivity.kt
+++ app/src/main/java/com/example/nbcamp_15ya_sns/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.nbcamp_15ya_sns
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        val button = findViewById<ImageView>(R.id.main_imageView5)
+
+        button.setOnClickListener {
+            val intent = Intent(this, MyPageActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
