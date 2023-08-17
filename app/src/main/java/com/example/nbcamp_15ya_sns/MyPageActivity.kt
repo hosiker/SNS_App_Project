1197bf917b70e25aad4a3be604264d4f10dd6ee9
송규진 <songu9610@gmail.com>
2023-08-17 19:46:09 +0900

    [FEAT] : ⚙️  마이페이지로 인텐트 부여

--- app/src/main/java/com/example/nbcamp_15ya_sns/MyPageActivity.kt
+++ app/src/main/java/com/example/nbcamp_15ya_sns/MyPageActivity.kt
@@ -8,5 +8,11 @@ class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.my_page_activity)
+
+        val userME = intent.getSerializableExtra("user") as User
+        val myIMG = intent.getSerializableExtra("img")
+
+
+
     }
 }
